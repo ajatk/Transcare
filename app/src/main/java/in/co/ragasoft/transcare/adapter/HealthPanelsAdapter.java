94ebd93914d595d1b9ce183767605e2fcecbb1ef
ajatk <ajat.k.web@gmail.com>
2018-03-13 18:24:49 +0530

    Merge branch 'master1' of E:\git\Transcare with conflicts.

---
+++ app/src/main/java/in/co/ragasoft/transcare/adapter/HealthPanelsAdapter.java
@@ -0,0 +1,8 @@
+package in.co.ragasoft.transcare.adapter;
+
+/**
+ * Created by Rajat on 3/13/2018.
+ */
+
+public class HealthPanelsAdapter {
+}
---
+++ app/src/main/java/in/co/ragasoft/transcare/modelClasses/ModelClass.java
@@ -0,0 +1,8 @@
+package in.co.ragasoft.transcare;
+
+/**
+ * Created by Rajat on 3/13/2018.
+ */
+
+public class ModelClasses {
+}
